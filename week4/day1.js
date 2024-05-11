ee6b2ac027e2b6a0097c2ddea92028fbc8befeda
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-11 13:58:46 +0500

    js day 1 variables and datatypes

---
+++ week4/day1.js
@@ -0,0 +1,64 @@
+// console.log("file attached with html day 1")
+
+// alert("pop notifications")
+
+// basic maths => arithmetical operations => CPU
+console.log(4+5)
+console.log(4*5)
+console.log(4-5)
+console.log(8/4) // division
+console.log(13%5) // remiander
+document.getElementById("mathAns").innerHTML = 12+3
+
+// RAM:
+console.log("x")
+
+let eng = "any english sentence" // 9th
+console.log(eng) // eng ram pe nhi mila, error
+
+eng = "zindagi k nasheb o faraz mein aesy lamhat bhi lty hain " // 10th
+
+console.log(eng) // nechy wali
+
+// console.log(urdu)
+
+// var urdu = "ati hai urdu zubaan aty aty" // tab tab ki tabnhi deki jati
+
+const pie = 3.14
+console.log(pie) // 3.14
+
+
+// pie = 444
+// console.log(pie)
+
+let studentName = "bilal" // 
+studentName = 123
+console.log(studentName)
+console.log(typeof(studentName))
+
+
+// let withSpace = 123
+// console.log(withSpace)
+
+// let 1name = 123
+
+let na2me = "faizan"
+console.log(na2me)
+
+let under_score= 123
+console.log(under_score)
+
+let bestCase = "camelCase"
+console.log(bestCase)
+console.log(typeof(bestCase))
+
+let bilal = "18" // 130
+console.log(typeof(bilal))
+
+let convertedIntoNum = parseInt(bilal)
+console.log(typeof(convertedIntoNum))
+
+let origNum = 18 // num
+console.log(typeof(origNum))
+let convertedIntoString = origNum.toString()
+console.log(typeof(convertedIntoString)) // str
