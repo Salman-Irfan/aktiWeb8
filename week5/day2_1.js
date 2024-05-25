b63ec6be4ff302cdbc2e396b2919e554428687c1
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-25 11:43:10 +0500

    functions repeat

---
+++ week5/day2_1.js
@@ -0,0 +1,9 @@
+let name1 = `anas`
+console.log(`${name1} ko dollar ki smj nhi ai`) // anas
+
+const markAbsent = (studentName) => {
+    return (`${studentName} is absent`)
+}
+
+let koi =  markAbsent(`bilal`)
+console.log(koi)
