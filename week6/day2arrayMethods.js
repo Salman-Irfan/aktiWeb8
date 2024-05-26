b5023ed85815292e69d0acb7456b5e4e97feed93
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-26 12:07:49 +0500

    arrays methods satart last and custom insertions and deletions

---
+++ week6/day2arrayMethods.js
@@ -0,0 +1,28 @@
+// array k mashoor o maroof methods
+// insertion and deletion from last
+const bilalMarks = [12, 18, 4, 5, 6, 87]
+console.log(bilalMarks)
+bilalMarks.push(65)
+console.log(bilalMarks)
+
+const arrayOfFruits = [`mango`, `strawberry`, `apple`, `leechi`, `falsa`, `tarbooz`, `bhindi`]
+console.log(arrayOfFruits)
+const mjyKyuNikala =  arrayOfFruits.pop()
+console.log(mjyKyuNikala)
+console.log(arrayOfFruits)
+
+// insert and deletions from start
+const arrayOfSabzis = [`bhindi`, `karelay`, `bengan`, `aalo`, `adrak`]
+console.log(arrayOfSabzis)
+arrayOfSabzis.unshift(`piyaaz`)
+console.log(arrayOfSabzis)
+const mjyStartSyKyuNikala = arrayOfSabzis.shift()
+console.log(mjyStartSyKyuNikala)
+console.log(arrayOfSabzis)
+
+// insertions and deletions hamari marzi ki jga sy
+const bilalFriends = [`faizan`, `anas`, `zeeshan`, `abdullah`]
+// bilalFriends.splice(1,0,`shabana`, `nasreen`)
+// console.log(bilalFriends)
+bilalFriends.splice(1,2, `rizwan`, `talha`)
+console.log(bilalFriends)
