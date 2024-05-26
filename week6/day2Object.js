663f3fbe8c25a023657f785848303ac72f865d4c
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-05-26 11:19:42 +0500

    objects basics

---
+++ week6/day2Object.js
@@ -0,0 +1,48 @@
+// 123 (numbers) as a value
+// "abc" (strings) as a value
+// true (boolean) as a value
+
+// functions => name => ram
+
+// arrays
+
+// object => heap memory k address sy
+
+const firstObject = {
+    num: 123,
+    anyStr: "abc",
+    isBoolean: true
+}
+
+const secondObj = firstObject
+console.log(secondObj) // first object wala e aye ga ?
+secondObj.num = 789
+console.log(secondObj) // ab num ki value kia ho gi 123, 789 ?
+console.log(firstObject) // first obj mei sec obj k num ki value change krny kia 1st waly mei change ho gi ? 123, 789
+
+
+console.log(firstObject)
+console.log(firstObject.num) // 123
+console.log(firstObject.anyStr) // abc
+console.log(firstObject.isBoolean) // true
+
+console.log(typeof(firstObject))
+
+const demoArray = [1,2,3,4]
+console.log(typeof(demoArray)) // object
+
+
+const objHtml = document.getElementById(`objHtml`)
+
+objHtml.innerHTML = `
+    <h1>${firstObject.num}</h1>
+    <h2>${firstObject.anyStr}</h2>
+    <h3>${firstObject.isBoolean}</h3>
+`
+
+let varOne = 123 // 123
+let varTwo = varOne // 123
+varTwo = 789 // 789
+console.log(varOne) // 123 / 789?
+
+
